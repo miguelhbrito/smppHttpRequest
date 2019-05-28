ec684af9f863ae929c7d1e13ae8312e38146c8c7
Miguel Henrique de Brito Pereira <miguelhbrito@gmail.com>
2019-05-28 15:44:20 -0300

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo /usr/bin/java -jar /smppartifact/target/smppartifact-0.0.1-SNAPSHOT.jar server
+sudo /usr/bin/java -jar /smppHttpRequest/target/smppartifact-0.0.1-SNAPSHOT.jar server
